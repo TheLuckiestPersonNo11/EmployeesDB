de088e95542b6f05789dc72def69728c03540099
Illia Koptiev <20ikop20@gmail.com>
2024-07-13 18:53:04 +0300

    Delete MainProject25 directory

--- MainProject25/Model/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MainProject25.Model
-{
-    internal class Item
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Identifier { get; set; }
-    }
-}
